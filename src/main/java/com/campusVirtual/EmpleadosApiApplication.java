f309cb3ccdea719bf20747f331b340a04e002c88
Esteban <esabinio13@gmail.com>
2023-03-15 12:21:16 -0300

    back

--- src/main/java/com/campusVirtual/EmpleadosApiApplication.java
+++ src/main/java/com/campusVirtual/EmpleadosApiApplication.java
@@ -7,13 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 import com.campusVirtual.dto.UserRegisterDto;
 import com.campusVirtual.model.Alumno;
@@ -85,22 +82,7 @@ public class EmpleadosApiApplication {
 		};
 	
 	}
-/* 
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:4200/login"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin,*", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin,*,*", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}*/
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
     		return new WebMvcConfigurer() {
--- src/main/java/com/campusVirtual/exception/Error.java
+++ src/main/java/com/campusVirtual/exception/Error.java
@@ -14,7 +14,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter @RequiredArgsConstructor @NoArgsConstructor @ToString
+@Getter 
+@Setter 
+@RequiredArgsConstructor 
+@NoArgsConstructor 
+@ToString
 public class Error {
 
 	@NonNull
